98c5bc3a755913c47b300e1dda6013c2278d2546
Lê Gia Hoàng <giahoangth@gmail.com>
2017-09-21 22:31:40 +0700

    Singleton with lazy initialization

--- src/Singleton/SingletonClass.java
+++ src/Singleton/SingletonClass.java
@@ -4,13 +4,16 @@ package Singleton;
  * Created by hoang on 21/09/2017.
  */
 public class SingletonClass {
-    private static volatile SingletonClass singleton  = new SingletonClass();
+    private static SingletonClass singleton;
 
     //private Contructor
     private SingletonClass(){}
 
     public static SingletonClass getInstance(){
-        return  singleton;
+        if(singleton == null){
+            singleton = new SingletonClass();
+        }
+        return singleton;
     }
     public void printSomething(){
         System.out.println("Life is hard");
