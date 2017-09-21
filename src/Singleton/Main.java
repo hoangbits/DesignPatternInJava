4475d230542bb39863edd45c965a4284ed6ea808
Lê Gia Hoàng <giahoangth@gmail.com>
2017-09-21 22:43:22 +0700

    Singleton test- return same object-samehash

--- src/Singleton/Main.java
+++ src/Singleton/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         SingletonClass singletonClass = SingletonClass.getInstance();
-        singletonClass.printSomething();
+        SingletonClass singletonClass1 = SingletonClass.getInstance();
+        System.out.println("Instance 1 hash:"+singletonClass.hashCode());
+        System.out.println("Instance 2 hash:"+singletonClass1.hashCode());
     }
 }
